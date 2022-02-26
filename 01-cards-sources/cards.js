c62395ee952f07e31cfa612a3f0b07de10bfcf20
Pasha <amberswcx@gmail.com>
2022-02-26 20:04:10 +0400

    feat: add random choosing of slide

--- 01-cards-sources/cards.js
+++ 01-cards-sources/cards.js
@@ -1,11 +1,17 @@
-const slides = document.querySelectorAll('.slide');
-slides.forEach(slide => slide.addEventListener('click', showSlide))
+function slidesPlugin(active) {
+  const slides = document.querySelectorAll('.slide');
+  slides.forEach(slide => slide.addEventListener('click', showSlide))
 
-function showSlide() {
-  clearActive();
-  this.classList.add('active');
+  slides[active].classList.add('active');
+
+  function showSlide() {
+    clearActive();
+    this.classList.add('active');
+  }
+
+  function clearActive() {
+    slides.forEach(slide => slide.classList.remove('active'))
+  }
 }
 
-function clearActive() {
-  slides.forEach(slide => slide.classList.remove('active'))
-}
+slidesPlugin(Math.floor(Math.random() * 4));
