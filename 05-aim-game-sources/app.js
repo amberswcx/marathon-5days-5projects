17a243ff7eeeeaa2607b44cbd01ccb717a58ca07
Pasha <amberswcx@gmail.com>
2022-03-03 18:16:27 +0400

    feat: add function for screen with select time

--- 05-aim-game-sources/app.js
+++ 05-aim-game-sources/app.js
@@ -1,9 +1,19 @@
 const startButton = document.querySelector('.start');
 const screens = document.querySelectorAll('.screen');
+const timeList = document.querySelector('.time-list');
+let time = 0;
 
 startButton.addEventListener('click', startGame);
+timeList.addEventListener('click', startGameWithTimer);
 
 function startGame(e) {
   e.preventDefault();
   screens[0].classList.add('up');
+}
+
+function startGameWithTimer(e) {
+  if (e.target.classList.contains('time-btn')) {
+    time = +(e.target.dataset.time);
+    screens[1].classList.add('up');
+  }
 }
